1b506fd9d3879a228995acd9b24a3ab1774aaf9c
unknown <infinity_michael@yahoo.com>
2020-04-08 07:38:23 -0700

    edited the challenge one task

--- src/estimator.js
+++ src/estimator.js
@@ -1,41 +1,41 @@
 const timeEstimator = (periodType, timeToElapse) => {
-    switch (periodType) {
-        case 'days':
-            const unitInDays = Math.floor(timeToElapse / 3);
-            return (result = Math.pow(2, unitInDays));
+  switch (periodType) {
+    case 'days':
+      const unitInDays = Math.floor(timeToElapse / 3);
+      return (result = Math.pow(2, unitInDays));
 
-        case 'weeks':
-            const weeksToDays = timeToElapse * 7;
-            const unitInMonths = Math.floor(weeksToDays / 3);
-            return (result = Math.pow(2, unitInMonths));
+    case 'weeks':
+      const weeksToDays = timeToElapse * 7;
+      const unitInMonths = Math.floor(weeksToDays / 3);
+      return (result = Math.pow(2, unitInMonths));
 
-        case 'months':
-            const monthsToDays = timeToElapse * 30;
-            const unitInWeeks = Math.floor(monthsToDays / 3);
-            return (result = Math.pow(2, unitInWeeks));
+    case 'months':
+      const monthsToDays = timeToElapse * 30;
+      const unitInWeeks = Math.floor(monthsToDays / 3);
+      return (result = Math.pow(2, unitInWeeks));
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 };
 
 const covid19ImpactEstimator = (data) => {
-    const { reportedCases, periodType, timeToElapse } = data;
-    const impact = {};
-    const severeImpact = {};
-    impact.currentlyInfected = reportedCases * 10;
-    timeEstimator(periodType, timeToElapse);
-    impact.infectionsByRequestedTime = impact.currentlyInfected * result;
-    severeImpact.currentlyInfected = reportedCases * 50;
+  const { reportedCases, periodType, timeToElapse } = data;
+  const impact = {};
+  const severeImpact = {};
+  impact.currentlyInfected = reportedCases * 10;
+  timeEstimator(periodType, timeToElapse);
+  impact.infectionsByRequestedTime = impact.currentlyInfected * result;
+  severeImpact.currentlyInfected = reportedCases * 50;
 
-    timeEstimator(periodType, timeToElapse);
-    severeImpact.infectionsByRequestedTime =
-        severeImpact.currentlyInfected * result;
-    return {
-        data,
-        impact,
-        severeImpact
-    };
+  timeEstimator(periodType, timeToElapse);
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * result;
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
