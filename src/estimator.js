889729228356fc494e1c5559466876aee4275591
unknown <infinity_michael@yahoo.com>
2020-04-08 12:08:55 -0700

    testing challenge 1 -3

--- src/estimator.js
+++ src/estimator.js
@@ -15,35 +15,56 @@ const timeEstimator = (periodType, timeToElapse) => {
   return result;
 };
 
+const flightInUSD = (periodType, timeToElapse, avgDailyIncome) => {
+  let Indays;
+  if (periodType === 'days') {
+    Indays = 0.65 * avgDailyIncome * timeToElapse;
+  } else if (periodType === 'weeks') {
+    Indays = 0.65 * avgDailyIncome * timeToElapse * 7;
+  } else if (periodType === 'months') {
+    Indays = 0.65 * avgDailyIncome * timeToElapse * 30;
+  }
+  return Indays;
+}
+
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, periodType, timeToElapse, totalHospitalBeds } = data;
-  const impact = {
-
-  };
-  const severeImpact = {
-
-  };
- 
-/**
-* challenge one
-*/
+  const { reportedCases,
+    periodType,
+    timeToElapse,
+    totalHospitalBeds,
+    region: { avgDailyIncomeInUSD }
+  } = data;
+  const impact = {};
+  const severeImpact = {};
+  /**
+   * challenge one
+   */
   impact.currentlyInfected = reportedCases * 10;
   const timeEstimate = timeEstimator(periodType, timeToElapse);
   impact.infectionsByRequestedTime = impact.currentlyInfected * timeEstimate;
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * timeEstimate;
- 
-/**
-* challenge two
-*/
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * timeEstimate;
+  /**
+   * challenge two
+   */
   impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
   const hospitalBeds = totalHospitalBeds * 0.35;
   impact.HospitalBedsByRequestedTime = hospitalBeds - impact.severeCasesByRequestedTime;
   severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
-  const { severeCasesByRequestedTime } = severeImpact;
+  const { severeCasesByRequestedTime, infectionsByRequestedTime } = severeImpact;
   severeImpact.HospitalBedsByRequestedTime = hospitalBeds - severeCasesByRequestedTime;
+  /**
+    * challenge three
+    */
+  impact.casesForICUByRequestedTiime = impact.infectionsByRequestedTime * 0.05;
+  severeImpact.casesForICUByRequestedTiime = infectionsByRequestedTime * 0.05;
+  impact.casesForVentilatorsRequestedTiime = impact.infectionsByRequestedTime * 0.02;
+  severeImpact.casesForVentilatorsRequestedTiime = infectionsByRequestedTime * 0.02;
+  const getFlightUSD = flightInUSD(periodType, timeToElapse, avgDailyIncomeInUSD);
+  impact.dollarsInFlight = impact.infectionsByRequestedTime * getFlightUSD;
+  severeImpact.dollarsInFlight = infectionsByRequestedTime * getFlightUSD;
 
- 
   return {
     data,
     impact,
