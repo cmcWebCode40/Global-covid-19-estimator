9a4d115f86316876aba1bc23f8bd8637a218e8ba
unknown <infinity_michael@yahoo.com>
2020-04-08 09:57:55 -0700

    completed challenge two

--- src/estimator.js
+++ src/estimator.js
@@ -16,14 +16,26 @@ const timeEstimator = (periodType, timeToElapse) => {
 };
 
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, periodType, timeToElapse } = data;
+  const { reportedCases, periodType, timeToElapse, totalHospitalBeds } = data;
   const impact = {};
   const severeImpact = {};
+  //challenge one 
   impact.currentlyInfected = reportedCases * 10;
   const timeEstimate = timeEstimator(periodType, timeToElapse);
   impact.infectionsByRequestedTime = impact.currentlyInfected * timeEstimate;
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * timeEstimate;
+  severeImpact.infectionsByRequestedTime =severeImpact.currentlyInfected * timeEstimate;
+
+
+  //challenge Two 
+  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+  const hospitalBeds = totalHospitalBeds * 0.35;
+  impact.HospitalBedsByRequestedTime = hospitalBeds - impact.severeCasesByRequestedTime;
+  severeImpact.severeCasesByRequestedTime =severeImpact.infectionsByRequestedTime * 0.15;
+  const {severeCasesByRequestedTime} = severeImpact;
+  severeImpact.HospitalBedsByRequestedTime = hospitalBeds - severeCasesByRequestedTime;
+
+ 
   return {
     data,
     impact,
